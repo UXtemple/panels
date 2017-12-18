e208901527d9c0ad349a12d60102e317dd89a961
Darío Javier Cravero <dario@uxtemple.com>
2017-12-18 13:38:41 +0000

    feat: I18n support for RCE

--- blocks/input.js
+++ blocks/input.js
@@ -1,25 +1,27 @@
-import React from 'react'
-import toCSS from 'style-to-css'
-import uniqueId from 'mini-unique-id'
+import React from 'react';
+import PropTypes from 'prop-types';
+import toCSS from 'style-to-css';
+import uniqueId from 'mini-unique-id';
 
 const PLACEHOLDER_PREFIXES = [
   '::-webkit-input-placeholder',
   '::-moz-placeholder',
   ':-ms-input-placeholder',
   ':placeholder-shown',
-]
+];
 
 export default class Input extends React.Component {
   constructor(props) {
-    super(props)
-    this.id = `Input-${uniqueId()}`
+    super(props);
+    this.id = `Input-${uniqueId()}`;
   }
 
   render() {
-    const { id } = this
+    const {context, id} = this;
     const {
       disabled,
       onEnter,
+      placeholder,
       _ref,
       style,
       styleDisabled,
@@ -28,47 +30,46 @@ export default class Input extends React.Component {
       stylePlaceholder,
       styleWrapper,
       ...rest
-    } = this.props
+    } = this.props;
 
-    let finalStyle = style
+    let finalStyle = style;
 
-    const backgroundColor = (style && style.backgroundColor) || 'transparent'
-    const color = (style && style.color) || 'black'
+    const backgroundColor = (style && style.backgroundColor) || 'transparent';
+    const color = (style && style.color) || 'black';
     const inlineStyle = [
       `#${id}:-webkit-autofill {
       background-color: ${backgroundColor} !important;
       box-shadow: 0 0 0px 1000px ${backgroundColor} inset;
       color: ${color} !important;
     }`,
-    ]
+    ];
 
     if (stylePlaceholder) {
       PLACEHOLDER_PREFIXES.forEach(prefix => {
-        inlineStyle.push(`#${id}${prefix} {${toCSS(stylePlaceholder)}}`)
-      })
+        inlineStyle.push(`#${id}${prefix} {${toCSS(stylePlaceholder)}}`);
+      });
     }
 
     if (styleDisabled && disabled) {
       finalStyle = {
         ...style,
         ...styleDisabled,
-      }
+      };
     } else {
       if (styleHover) {
-        inlineStyle.push(`${inlineStyle} #${id}:hover {${toCSS(styleHover)}}`)
+        inlineStyle.push(`${inlineStyle} #${id}:hover {${toCSS(styleHover)}}`);
       }
       if (styleFocus) {
-        inlineStyle.push(`#${id}:focus {${toCSS(styleFocus)}}`)
+        inlineStyle.push(`#${id}:focus {${toCSS(styleFocus)}}`);
       }
     }
 
-    let onKeyUp
+    let onKeyUp;
     if (typeof onEnter !== 'undefined') {
       /* eslint-disable no-console */
-      const finalOnEnter = typeof onEnter === 'function'
-        ? onEnter
-        : () => console.log(onEnter)
-      onKeyUp = event => event.key === 'Enter' && finalOnEnter(event)
+      const finalOnEnter =
+        typeof onEnter === 'function' ? onEnter : () => console.log(onEnter);
+      onKeyUp = event => event.key === 'Enter' && finalOnEnter(event);
     }
 
     return (
@@ -80,10 +81,11 @@ export default class Input extends React.Component {
           id={id}
           onKeyUp={onKeyUp}
           ref={_ref}
+          placeholder={context.i18n ? context.i18n.t(placeholder) : placeholder}
           style={finalStyle}
         />
       </div>
-    )
+    );
   }
 }
 
@@ -93,4 +95,10 @@ Input.defaultProps = {
   styleHover: {},
   styleWrapper: {},
   type: 'text',
-}
+};
+
+Input.contextTypes = {
+  i18n: PropTypes.shape({
+    t: PropTypes.func.isRequired,
+  }),
+};
--- blocks/text.js
+++ blocks/text.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Text = ({ style, text, ...props }) => {
+const Text = ({style, text, ...props}, context) => {
   const finalStyle = {
     whiteSpace: 'pre-line',
     ...style,
   };
   return (
     <div {...props} style={finalStyle}>
-      {text}
+      {context.i18n ? context.i18n.t(text) : text}
     </div>
   );
+};
+Text.contextTypes = {
+  i18n: PropTypes.shape({
+    t: PropTypes.func.isRequired,
+  }),
 };
 
 Text.defaultProps = {
